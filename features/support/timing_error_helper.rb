2ffa3f9c5a0c89570b15ced4a2fac3f49444f46e
Caleb Buxton <me@cpb.ca>
2013-08-30 09:33:13 -0700

    Travis fixes
    don't pry on travis
    
    trying to fix timing error on travis
    
    Staring xvfb for travis
    
    http://about.travis-ci.org/docs/user/gui-and-headless-browsers/ as a
    solution to https://travis-ci.org/cpb/the_wizard_of_api/builds/10808701

--- features/support/timing_error_helper.rb
+++ features/support/timing_error_helper.rb
@@ -3,6 +3,8 @@ unless defined?TimingErrorHelper
     def avoid_timing_errors
       begin
         yield
+      rescue Timeout::Error => e
+        retry
       rescue Capybara::Webkit::InvalidResponseError => e
         if e.message.include?("Unable to load URL")
           retry
